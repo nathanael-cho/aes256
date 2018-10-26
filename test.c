bb194f9ef7d2f65f64cfed5eca1da207ecd3fb17
Nathanael Cho <nathanael.a.cho@gmail.com>
2018-10-26 17:15:19 -0400

    More accurate variable name

--- test.c
+++ test.c
@@ -14,27 +14,27 @@ void dump(char* prefix, uint8_t* buffer) {
 
 void simple_test() {
     aes256_keys keys;
-    uint8_t round_key[32];
+    uint8_t seed_key[32];
     uint8_t buffer[16];
 
     for (uint8_t i = 0; i < sizeof(buffer); i++) {
         buffer[i] = i * 16 + i;
     }
-    for (uint8_t i = 0; i < sizeof(round_key);i++) {
-        round_key[i] = i;
+    for (uint8_t i = 0; i < sizeof(seed_key);i++) {
+        seed_key[i] = i;
     }
 
     dump("Text: ", buffer);
-    dump("Key: ", round_key);
+    dump("Key: ", seed_key);
     printf("---\n");
 
-    aes256_initialize(&keys, round_key);
+    aes256_initialize(&keys, seed_key);
     aes256_encrypt(&keys, buffer);
 
     dump("Encoding: ", buffer);
     printf("Expected Encoding: 8e a2 b7 ca 51 67 45 bf ea fc 49 90 4b 49 60 89\n");
 
-    aes256_initialize(&keys, round_key);
+    aes256_initialize(&keys, seed_key);
     aes256_decrypt(&keys, buffer);
     dump("Decrypted: ", buffer);
 
