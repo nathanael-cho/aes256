71c07acdf9165ec3f0248d1409e6136b34b501b2
Nathanael Cho <nathanael.a.cho@gmail.com>
2018-11-23 15:12:20 -0500

    Got rid of unnecessary imports

--- aes256.h
+++ aes256.h
@@ -11,13 +11,11 @@
 
 #include <fcntl.h>
 #include <pwd.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-#include <termios.h>
 #include <unistd.h>
 
 #include "include/infrastructure.h"
